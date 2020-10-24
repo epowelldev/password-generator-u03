0483050633d4a08104cb530b2049e5a728306b2d
Erickson Powell <ericksonpowell@gmail.com>
2020-10-24 16:44:02 -0700

    added combining character selection arrays based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,59 @@ var generateBtn = document.querySelector("#generate");
 // GLobal vars:
 //Arrays: list of numbers, lowercase letters, UPPERCASE LETTERS, and special characters
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var letterslower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lettersUPPER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const letterslower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lettersUPPER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-// prompt for password length
+// Function generatePassword:
+function generatePassword() {
 
-var passLength = parseInt(prompt("How many characters do you want in your password? \n Enter a number: 8 - 128"))
+  var characterSelection = [];
 
-// prompt for types of characters used: numbers, lowercase, UPPERCASE, special characters:
-// ok on confirm == true, cancel on confirm == false
-var useNumbers = confirm("Do you want to use numbers in your password?");
-var useLowercase = confirm("Do you want to use lowercase letters in your password?");
-var useUppercase = confirm("Do you want to use UPPERCASE LETTERS in your password?");
-var useSpecial = confirm("Do you want to use Special Characters (ie: !@#$%^ etc) in your password?");
+  // prompt for password length
+  var passLength = parseInt(prompt("How many characters do you want in your password? \n Enter a number: 8 - 128"))
+  // check to see if `passLength` is valid (from 8 to 128), if it isn't= alert the user and exit function
+  if (!(passLength >= 8 && passLength <= 128)) {
+    return alert("Not a valid entry of length!");
+  };
+
+  // prompt for types of characters used: numbers, lowercase, UPPERCASE, special characters:
+  // ok on confirm == true, cancel on confirm == false
+
+  var useNumbers = confirm("Do you want to use numbers in your password?");
+  // IF `useNumbers` is true, THEN add `numbers` to `characterSelection` array
+  if (useNumbers) {
+    characterSelection = characterSelection.concat(numbers);
+  }
+
+  var useLowercase = confirm("Do you want to use lowercase letters in your password?");
+  // IF `useLowercase` is true, THEN add `letterslower` to `characterSelection` array
+  if (useLowercase) {
+    characterSelection = characterSelection.concat(letterslower);
+  }
+
+  var useUppercase = confirm("Do you want to use UPPERCASE LETTERS in your password?");
+  // IF `useUppercase` is true, THEN add `lettersUPPER` to `characterSelection` array
+  if (useUppercase) {
+    characterSelection = characterSelection.concat(lettersUPPER);
+  }
+
+  var useSpecial = confirm("Do you want to use Special Characters (ie: !@#$%^ etc) in your password?");
+  // IF `useSpecial` is true, THEN add `specialCharacters` to `characterSelection` array
+  if (useSpecial) {
+    characterSelection = characterSelection.concat(specialCharacters);
+  }
 
 
+
+  console.log(passLength, useNumbers, useLowercase, useUppercase, useSpecial);
+  console.log(characterSelection);
+}
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
