5fcf013897f5156b0a73d618060a95c935c46dd1
Erickson Powell <ericksonpowell@gmail.com>
2020-10-24 15:50:53 -0700

    finsihed adding arrays for characters, finished prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,19 @@ var generateBtn = document.querySelector("#generate");
 
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letterslower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lettersUPPER = [];
-var specialCharacters = [];
+var lettersUPPER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // prompt for password length
 
-
+var passLength = parseInt(prompt("How many characters do you want in your password? \n Enter a number: 8 - 128"))
 
 // prompt for types of characters used: numbers, lowercase, UPPERCASE, special characters:
 // ok on confirm == true, cancel on confirm == false
-var useNumbers = confirm(useNumbers);
-var useLowercase = confirm(useLowercase);
-var useUppercase = confirm(useUppercase);
-var useSpecial = confirm(useSpecial);
+var useNumbers = confirm("Do you want to use numbers in your password?");
+var useLowercase = confirm("Do you want to use lowercase letters in your password?");
+var useUppercase = confirm("Do you want to use UPPERCASE LETTERS in your password?");
+var useSpecial = confirm("Do you want to use Special Characters (ie: !@#$%^ etc) in your password?");
 
 
 
